490592b63632f603dcfa487a75bbb17bb9ad76bc
xiyuan1985 <82679614@qq.com>
2019-09-23 17:16:05 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String args[]) {
+           System.out.println("HelloWorldd!");
+           System.out.println("1");
+           System.out.println("HelloWorldd!");
+           System.out.println("2");
+	}
+}
